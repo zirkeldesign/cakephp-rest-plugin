284d300122d0e264c20a97ae11a0c41c2b84ab6b
Kevin van Zonneveld <kevin@true.nl>
2009-11-04 16:45:50 +0100

    Rest tryout

---
+++ controllers/components/rest.php
@@ -0,0 +1,74 @@
+<?php
+Class RestComponent extends Object{
+    public $components = array('RequestHandler');
+    public $Controller;
+
+    public $codes = array(
+        200 => 'OK',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Time-out',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Time-out',
+    );
+
+    protected $_settings = array(
+        // Passed as Component options
+        'extensions' => array('xml', 'json'),
+        'reUseNormalView' => true,
+
+        // Passed as Both Helper & Component options
+        'debug' => '0',
+
+        // Passed as Helper options
+        'method' => null,
+    );
+
+    public function initialize (&$Controller, $settings=array()) {
+        $this->Controller = &$Controller;
+        $this->_settings = am($this->_settings, $settings);
+
+        // Make it an integer always
+        $this->_settings['debug'] = (int)$this->_settings['debug'];
+
+        // Setup the controller so it can use
+        // the view inside this plugin
+        $this->Controller->plugin = 'rest';
+    }
+
+    public function startup (&$Controller) {
+
+    }
+    
+    public function beforeRender (&$Controller) {
+        $this->Controller = &$Controller;
+
+        $this->Controller->helpers['Rest.RestXml'] = $this->_settings;
+        
+        if (in_array($this->Controller->params['url']['ext'], $this->_settings['extensions'])) {
+            // Pass vars on to views & helpers
+            Configure::write('debug', $this->_settings['debug']);
+            $this->Controller->set('debug', $this->_settings['debug']);
+
+            // View from user's app
+            if ($this->_settings['reUseNormalView']) {
+                $viewPath = $this->Controller->viewPath;
+                foreach ($this->_settings['extensions'] as $ext) {
+                    $viewPath = str_replace('/'.$ext, '', $viewPath);
+                }
+
+                $this->Controller->viewPath = $viewPath;
+            }
+        }
+    }
+}
+?>
---
+++ views/layout/xml/index.ctp
@@ -0,0 +1,3 @@
+<?php
+echo $rest_xml->serialize($content_for_layout);
+?>
